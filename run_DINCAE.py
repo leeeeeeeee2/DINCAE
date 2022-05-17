6eb2716da0cd2f0ccdb1e4d1f224ea18d97b8eec
LeeQQQQQQQ <37931523+leeeeeeeee2@users.noreply.github.com>
2022-05-17 16:20:44 +0800

    Update run_DINCAE.py

--- run_DINCAE.py
+++ run_DINCAE.py
@@ -8,6 +8,10 @@ from sys import argv
 # varname = "SST"
 # outdir = "/path/to/output/dir"
 
+if len(argv)!=4:
+  print('infile,filename,varname,outdir')
+  exit(1)
+
 infile,filename,varname,outdir=argv
 
 DINCAE.reconstruct_gridded_nc(filename,varname,outdir)
