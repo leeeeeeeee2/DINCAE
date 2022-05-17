8cb276aba7b8907b51e668302991ab8032dc4c48
LeeQQQQQQQ <37931523+leeeeeeeee2@users.noreply.github.com>
2022-05-17 16:18:58 +0800

    use cmd

--- run_DINCAE.py
+++ run_DINCAE.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import DINCAE
+from sys import argv
 
-filename = "/path/to/file.nc"
-varname = "SST"
-outdir = "/path/to/output/dir"
+# filename = "/path/to/file.nc"
+# varname = "SST"
+# outdir = "/path/to/output/dir"
+
+infile,filename,varname,outdir=argv
 
 DINCAE.reconstruct_gridded_nc(filename,varname,outdir)
